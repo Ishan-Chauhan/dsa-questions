33d068bb3d3317ec69b14fd6b6251e84e36aac09
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2022-11-30 06:27:40 +0530

    Time: 69 ms (36.74%), Space: 7.3 MB (76.72%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,27 +3,28 @@ public:
     int change(int amount, vector<int>& coins) {
         //sort(coins.begin(),coins.end());
         int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        // vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
         // int ans = sol(coins,amount,n-1,dp);
-        
+        vector<int> prev(amount+1), curr(amount+1);
         for(int i=0;i<=n;i++)
         {
             for(int j=0;j<=amount;j++)
             {
-                if(j==0) dp[i][j]=1;
+                if(j==0) curr[j]=1;
                 
-                else if(i==0) dp[i][j]=0;
+                else if(i==0) curr[j]=0;
                 
                 else
                 {
-                    if(j<coins[i-1]) dp[i][j] = dp[i-1][j];
+                    if(j<coins[i-1]) curr[j] = prev[j];
                     else
                     {
-                        dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                        curr[j] = prev[j] + curr[j-coins[i-1]];
                     }
                 }
             }
+            prev = curr;
         }
-        return dp[n][amount];
+        return curr[amount];
     }
 };
