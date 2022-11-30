877deff2f66e761245946440955e89f5d042e10a
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2022-11-30 06:25:03 +0530

    Time: 110 ms (19.97%), Space: 18.6 MB (32.80%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        //sort(coins.begin(),coins.end());
+        int n=coins.size();
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        // int ans = sol(coins,amount,n-1,dp);
+        
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=amount;j++)
+            {
+                if(j==0) dp[i][j]=1;
+                
+                else if(i==0) dp[i][j]=0;
+                
+                else
+                {
+                    if(j<coins[i-1]) dp[i][j] = dp[i-1][j];
+                    else
+                    {
+                        dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                    }
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+};
