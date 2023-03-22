9ccc4e288c4edb385fc944dd670a03b5f77247f5
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2023-03-22 11:46:19 +0530

    Time: 4 ms (68.46%), Space: 6.7 MB (21.93%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string getHint(string s, string g) {
+        int n = g.size();
+        map<char,int> m, mp;
+        int a=0, b=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]==g[i]) a++;
+            // else
+            // {
+            //     m[s[i]]++;
+            //     if(m[g[i]]>0) 
+            //     {
+            //         b++;
+            //         m[g[i]]--;
+            //     }
+            //     else mp[g[i]]++;
+            // }
+            else
+            {
+                m[s[i]]++;
+                mp[g[i]]++;
+            }
+        }
+        for(auto it: mp)
+        {
+            // cout<< it.first<<" ";
+            if(m[it.first]>0)
+            {
+                int x = min(m[it.first],mp[it.first]);
+                b += x;
+            }
+        }
+        return to_string(a)+"A"+to_string(b)+"B";
+    }
+};
