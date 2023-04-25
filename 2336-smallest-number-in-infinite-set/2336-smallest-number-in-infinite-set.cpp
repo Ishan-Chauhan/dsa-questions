0637d42a2e4ba3c34a2129d14cf50856ca1a9b95
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2023-04-25 17:37:55 +0530

    Time: 120 ms (48.02%), Space: 39.4 MB (56.22%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -0,0 +1,30 @@
+class SmallestInfiniteSet {
+public:
+    map<int,int> m;
+    int c=1;
+    SmallestInfiniteSet() {
+        
+    }
+    
+    int popSmallest() {
+        int y=c;
+        m[c]=-1;
+        while(m[c]<0)    c++;
+        return y;
+    }
+    
+    void addBack(int num) {
+        if(m[num]==-1)
+        {
+            m[num]=0;
+        }
+        if(num<c) c = num;
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
