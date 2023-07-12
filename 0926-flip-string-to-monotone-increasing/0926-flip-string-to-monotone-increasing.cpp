03311a3c39f9093148cadd9f38e1e930117bec15
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2023-07-12 18:10:36 +0530

    Time: 25 ms (75.70%), Space: 11.1 MB (45.40%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n = s.size();
-        
-        vector<int> dp(n+1,0);
-        dp[0]=0;
+        int ans=0;
         int ones=0;
         
         for(int i=1;i<=n;i++)
@@ -12,13 +10,12 @@ public:
             if(s[i-1]=='1') 
             {
                 ones++;
-                dp[i] = dp[i-1];
             }
             else
             {
-                dp[i] = min(ones, 1+dp[i-1]);
+                ans = min(ones, 1+ans);
             }
         }
-        return dp[n];
+        return ans;
     }
 };
