b52059b4ac512c98d9c29d96b278dfcfc2149b16
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2023-07-12 17:47:02 +0530

    Time: 235 ms (26.45%), Space: 61.5 MB (22.44%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,41 +1,52 @@
 class Solution {
-private:
-    bool dfs(int i,vector<vector<int>>& graph,vector<int>& vis,vector<int>& pathvis, vector<int>& res)
-    {
-        vis[i]=1;
-        pathvis[i]=1;
-        
-        // cout<<i<<endl;
-        for(auto node: graph[i])
-        {
-            if(!vis[node])
-            {
-                if(dfs(node, graph, vis, pathvis, res)==true) return true;
-            }
-            else if(pathvis[node]==1)
-            {
-                return true;
-            }
-        }
-        
-        // cout<<i<<endl;
-        res.push_back(i);
-        pathvis[i]=0;
-        return false;
-    }
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> vis(n,0), pathvis(n,0), res;
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph1) {
+        int n = graph1.size();
+        vector<vector<int>> graph(n, vector<int>());
+        vector<int> indegree(n,0), res;
         
-        for(int i=0; i<n; i++)
+        for(int i=0;i<n;i++)
         {
-            if(!vis[i])
+            for(auto it: graph1[i])
             {
-                dfs(i, graph, vis, pathvis, res);
+                graph[it].push_back(i);
+                // cout<<it<<" "<<i<<endl;
             }
         }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(auto node: graph[i]) indegree[node]++;
+        }
+        
+        queue<int> q;
+        for(int i=0;i<n;i++) 
+        {
+            if(indegree[i]==0) 
+            {
+                q.push(i);
+                res.push_back(i);
+            }
+        }
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            
+            for(auto x: graph[node])
+            {
+                indegree[x]--;
+                if(indegree[x]==0) 
+                {
+                    q.push(x);
+                    res.push_back(x);
+                }
+            }
+        }
+        
         sort(res.begin(), res.end());
+        
         return res;
     }
 };
