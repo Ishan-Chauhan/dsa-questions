e3206f790f360660c6f31ac9094862992b24e0ad
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2023-07-25 11:51:36 +0530

    Time: 38 ms (81.95%), Space: 12.2 MB (94.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,17 +3,17 @@ public:
     void sol(vector<vector<char>>& v,int x,int y)
     {
         //cout<<x<<" "<<y<<endl;
-        if(x==-1 || x==v.size() || y==-1 || y==v[0].size()) return;
+        if(x==-1 || x==v.size() || y==-1 || y==v[0].size() || v[x][y]!='1') return;
         
         v[x][y]='2';
         
-        if(x+1!=v.size() && v[x+1][y]=='1')
+        // if(x+1!=v.size() && v[x+1][y]=='1')
             sol(v,x+1,y);
-        if(x-1!=-1 && v[x-1][y]=='1')
+        // if(x-1!=-1 && v[x-1][y]=='1')
             sol(v,x-1,y);
-        if(y+1!=v[0].size() && v[x][y+1]=='1')
+        // if(y+1!=v[0].size() && v[x][y+1]=='1')
             sol(v,x,y+1);
-        if(y-1!=-1 && v[x][y-1]=='1')
+        // if(y-1!=-1 && v[x][y-1]=='1')
             sol(v,x,y-1);
     }
     int numIslands(vector<vector<char>>& grid) {
