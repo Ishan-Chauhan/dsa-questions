13515f8e06751f5e9aebce9ce090dc0d206e02fb
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2023-08-11 06:00:20 +0530

    Time: 15 ms (74.44%), Space: 16.5 MB (63.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> v(n,1e4+1);
-        v[n-1]=0;
-        for(int i=n-2;i>=0;i--)
+        for(int i=1;i<n-1;i++)
         {
-            for(int j=1;j<=nums[i];j++)
-            {
-                // cout<<1+v[i+j]<<" ";
-                v[i] = min(v[i],1+v[min(i+j,n-1)]);
-            }
-            // cout<<endl;
+            nums[i] = max(i+nums[i], nums[i-1]);
         }
-        return v[0];
+        
+        int id=0, ans=0;
+        while(id<n-1)
+        {
+            id = nums[id];
+            ans++;
+        }
+        
+        return ans;
     }
 };
