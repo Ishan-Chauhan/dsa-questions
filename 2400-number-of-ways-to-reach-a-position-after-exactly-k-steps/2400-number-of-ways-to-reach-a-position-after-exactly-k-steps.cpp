93d05ed61b8c8d26d0c6961f749749067a4912f7
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2024-01-02 19:41:33 +0530

    Time: 198 ms (36.19%), Space: 120.4 MB (51.11%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -5,7 +5,7 @@ public:
     {
         
         if(k==0 && s==e) return 1;
-        if(k<=0 || s<0 || s>2000) return 0;
+        if(abs(s-e)>k) return 0;
         
         if(dp[s][k]!=-1) return dp[s][k]%mod;
         
@@ -14,16 +14,12 @@ public:
     }
     int numberOfWays(int s, int e, int k) {
         
-        e=e-s;
+        e=abs(e-s);
         s=0;
         
-        if(e<0)
-        {
-            swap(e,s);
-            s*=-1;
-        }
         
         vector<vector<int>> dp(2001, vector<int>(k+1, -1));
+        
         return sol(s+1000, e+1000, k, dp)%mod;
     }
 };
