9afe4e62bbd2d16712f0f9b2ad249bb5c67719c5
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2024-01-05 18:06:28 +0530

    Time: 1182 ms (72.17%), Space: 408 MB (45.18%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
-    priority_queue<pair<string,int>> sol(vector<string>& nums, int n, int m, int k, int trim)
-    {
-        // cout<<"helo"<<endl;
-        priority_queue<pair<string,int>> pq;
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& q) {
+        
+        int n=q.size();
+        vector<int> res;
+        
         for(int i=0;i<n;i++)
         {
-            string temp="";
-            // for(int j=m-1;j>=m-trim;j--)
-            // {
-            //     temp = nums[i][j]+temp;
-            // }
+            int k=q[i][0];
+            int t=q[i][1];
+            priority_queue<pair<string, int>> pq;
             
-            for(int j=m-trim;j<=m-1;j++) temp+=nums[i][j];
-            // cout<<temp<<" ";
-            // int x = stoi(temp);
-            // cout<<temp<<" "<<i<<" "<<k<<endl;
-            pq.push({temp,i});
+            for(int j=0;j<nums.size();j++)
+            {
+                string str = nums[j].substr(nums[j].size()-t,t);
+                
+                pq.push({str, j});
+                
+                if(pq.size()>k) pq.pop();
+            }
             
-            if(pq.size()>k) pq.pop();
+            res.push_back(pq.top().second);
         }
         
-        return pq;
-    }
-    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        
-        int m = nums[0].size();
-        int q = queries.size();
-        int n = nums.size();
-        
-        vector<int> ans;
-        
-        for(int i=0; i<q ; i++)
-        {
-            int k = queries[i][0];
-            int trim = queries[i][1];
-            
-            priority_queue<pair<string,int>> pq = sol(nums,n,m,k, trim);
-            ans.push_back(pq.top().second);
-        }
-        
-        return ans;
+        return res;
     }
 };
