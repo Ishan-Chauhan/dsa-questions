929eaca40a1ef786c6955eb2dbaa9f705c6b6548
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2024-01-08 20:29:49 +0530

    Time: 58 ms (89.68%), Space: 67.4 MB (65.81%) - LeetHub

--- 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.cpp
+++ 0497-random-point-in-non-overlapping-rectangles/0497-random-point-in-non-overlapping-rectangles.cpp
@@ -28,35 +28,5 @@ public:
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution* obj = new Solution(rects);
- * vector<int> param_1 = obj->pick();
- 
- class Solution {
-public:
-    vector<int> area;
-    vector<vector<int>> v;
-
-    int solve(vector<int>& it){
-        return (it[2]-it[0]+1)*(it[3]-it[1]+1);
-    }
-
-    Solution(vector<vector<int>>& rects) {
-        v = rects;
-        int total_area=0;
-        for(auto it : rects){
-            total_area += solve(it); 
-            area.push_back(total_area);
-        }
-    }
-    
-    vector<int> pick() {
-        int x = random()%area.back() + 1;
-        int idx = lower_bound(area.begin(), area.end(), x) - area.begin();
-
-        auto r = v[idx];
-        x = random()%(r[2] - r[0] + 1) + r[0];
-        int y = random()%(r[3] - r[1] + 1) + r[1];
-        return {x,y}; 
-    }
-};
- 
- */
+ * vector<int> param_1 = obj->pick(); 
+*/
