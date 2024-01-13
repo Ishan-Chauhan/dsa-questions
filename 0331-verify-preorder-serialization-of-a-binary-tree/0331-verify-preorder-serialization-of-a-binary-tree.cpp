e81ca38085696b963d8b51299ea4b780cd6edbae
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2024-01-13 22:05:46 +0530

    Time: 5 ms (45.74%), Space: 7.2 MB (55.45%) - LeetHub

---
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValidSerialization(string pre) {
+        int n=pre.size();
+        stack<char> st;
+        
+        for(int i=0;i<n;i+=2)
+        {
+            if(pre[i]=='#')
+            {
+                // if(st.empty()) return false;
+                while(!st.empty() && st.top()==pre[i])
+                {
+                    st.pop();
+                    if(st.empty()) return false;
+                    st.pop();
+                }
+                st.push(pre[i]);
+            }
+            else
+            {
+                while(pre[i]>='0' && pre[i]<='9') 
+                {
+                    i++;
+                }
+                i--;
+                st.push('1');
+            }
+        }
+        return (st.size()==1 && st.top()=='#');
+    }
+};
