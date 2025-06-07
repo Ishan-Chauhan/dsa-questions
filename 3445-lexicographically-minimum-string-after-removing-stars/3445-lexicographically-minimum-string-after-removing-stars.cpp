83449817167e6c2e202c3668bb34179aeae248a5
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2025-06-07 13:42:05 +0530

    Time: 54 ms (82.47%), Space: 38.4 MB (23.7%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        int k,n=s.size();
+
+        vector<stack<int>> v(26);
+        vector<bool> take(n, true);
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='*')
+            {
+                take[i]=false;
+                for(int i=0;i<26;i++)
+                {
+                    if(!v[i].empty())
+                    {
+                        k = v[i].top();
+                        v[i].pop();
+                        take[k]=false;
+                        break;  
+                    }
+                }
+            }
+            else
+            {
+                v[s[i]-'a'].push(i);
+            }
+        }
+
+        string ans;
+        for(int i=0;i<n;i++)
+        {
+            if(take[i]) ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
