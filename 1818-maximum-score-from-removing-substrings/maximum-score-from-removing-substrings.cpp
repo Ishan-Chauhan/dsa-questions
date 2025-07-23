3e94e564644448591b38e18e1c05efe749bb83f7
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2025-07-23 14:27:52 +0530

    Time: 63 ms (34.05%) | Memory: 27 MB (70.02%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int point(string& s, char a, char b, int x)
+    {
+        int n=s.size(), ans=0;
+        stack<char> st;
+        for(int i=0;i<n;i++)
+        {
+            if(st.empty()) 
+            {
+                st.push(s[i]);
+                continue;
+            }
+            if(s[i]==b && st.top()==a)
+            {
+                st.pop();
+                ans += x;
+            }
+            else st.push(s[i]);
+        }
+        s="";
+        while(!st.empty())
+        {
+            s.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+    int maximumGain(string s, int x, int y) {
+        int ans=0;
+        string temp = s;
+
+        if(x>=y)
+        {
+            ans += point(temp, 'a', 'b', x);
+            ans += point(temp, 'a', 'b', y);
+        }
+        else 
+        {
+            ans += point(temp, 'b', 'a', y);
+            // cout<<temp;
+            ans += point(temp, 'b', 'a', x);
+        }
+
+        return ans;
+    }
+};
