23b0de48e2a5790585a345dcb861c38ce65d431f
Ishan-Chauhan <90429744+Ishan-Chauhan@users.noreply.github.com>
2025-08-14 12:40:19 +0530

    Time: 299 ms (33.97%), Space: 274.6 MB (23.22%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int numberOfWays(int n, int x) {
-        vector<int> dp(n+1);
-        dp[0]=1;
-        long long mod = 1e9 + 7;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        dp[0][0]=1;
+        long long mod = 1e9+7;
 
         for(int i=1;i<=n;i++)
         {
-            int t = pow(i, x);
-            if(t>n) break;
-
-            for(int j=n;j>=t;j--)
+            long long val = pow(i, x);
+            for(int j=0;j<=n;j++)
             {
-                dp[j] = (dp[j] + dp[j-t])%mod;
+                if(j>=val)
+                    dp[i][j] = (dp[i-1][j] + dp[i-1][j-val])%mod;
+                else
+                    dp[i][j] = dp[i-1][j];
             }
         }
-        return dp[n];
-        
+        return dp[n][n];
     }
 };
